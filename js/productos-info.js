a2c8f59d1e43d9465e14241a00cc3ea8649728a5
Enzo Gómez <enzoeegv@gmail.com>
2023-09-22 01:06:44 -0300

    productInfo avance

--- js/productos-info.js
+++ js/productos-info.js
@@ -1,24 +1,38 @@
 const contenedorTitulo = document.getElementById('contenedorTitulo');
 const contenedorTabla = document.getElementById('contenedor');
+const cuerpoTabla = document.getElementById('cuerpoTabla');
 
-const URL = 'json/caños.json'; 
+const URL = 'json/caños.json';
 
 
-function traerInfo(){
-    fetch('json/caños.json')
+function traerInfo() {
+  fetch(URL)
     .then(Response => Response.json())
-    .then(data =>{
-        console.log(data.titulo);
-        cambiarTitulo(data);
+    .then(data => {
+      console.log(data.titulo);
+      cambiarTitulo(data);
+      mostrarArticulos(data.objeto)
     })
 }
 
-function cambiarTitulo(array){
-    let titulo = document.createElement('h1');
-    titulo.textContent = array.titulo
-    contenedorTitulo.appendChild(titulo)
+function mostrarArticulos(array) {
+  array.forEach(element => {
+    cuerpoTabla.innerHTML += `
+        <tr>
+          <td data-label="Nombre">${element.nombre}</td>
+          <td data-label="Precio">${element.precioUnitario} ${element.moneda}</td>
+          <td data-label="En Stock">${element.publicado}</td>
+        </tr>
+        `
+  });
+}
+
+function cambiarTitulo(array) {
+  let titulo = document.createElement('h1');
+  titulo.textContent = array.titulo
+  contenedorTitulo.appendChild(titulo)
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    traerInfo();
+  traerInfo();
 })
